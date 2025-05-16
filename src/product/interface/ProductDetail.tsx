95ccd674212f7dd4858c83b57d4aca8cbb6ed74f
ALVARO <alvarocrispin0604@gmail.com>
2025-05-16 17:08:06 -0500

    feat: styling UI of product detail

--- src/product/interface/ProductDetail.tsx
+++ src/product/interface/ProductDetail.tsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Typography,
+    Button,
+    Grid,
+    Box
+} from '@mui/material'
 import type {Product} from "../domain/Product.ts";
 
 interface Props {
@@ -8,14 +17,50 @@ interface Props {
 
 export const ProductDetail: React.FC<Props> = ({ product, onBack }) => {
     return (
-        <div className="p-4">
-            <button onClick={onBack} className="mb-4 bg-gray-500 text-gray py-1 px-4 rounded">
-                Back
-            </button>
-            <h2 className="text-2xl font-bold">{product.title}</h2>
-            <img src={product.image} alt={product.title} className="w-full h-48 object-cover mb-4" />
-            <p className="text-gray-600">${product.price}</p>
-            <p className="mt-2">{product.description}</p>
-        </div>
+        <Box padding={4}>
+            <Button variant="outlined" onClick={onBack} sx={{ mb: 2 }}>
+                Volver
+            </Button>
+
+            <Card sx={{ padding: 2 }}>
+                <Grid container spacing={3}>
+                    {/* Image */}
+                    <Grid size={{ xs: 12, md: 5}}>
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            image={product.image}
+                            alt={product.title}
+                            sx={{ objectFit: 'contain', padding: 2 }}
+                        />
+                    </Grid>
+
+                    {/* Details */}
+                    <Grid size={{ xs: 12, md: 7}}>
+                        <CardContent>
+                            <Typography variant="h5" gutterBottom>
+                                {product.title}
+                            </Typography>
+
+                            <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+                                Categoría: {product.category}
+                            </Typography>
+
+                            <Typography variant="h6" color="primary" sx={{ fontWeight: 'bold' }} gutterBottom>
+                                S/ {product.price.toFixed(2)}
+                            </Typography>
+
+                            <Typography variant="body1" paragraph>
+                                {product.description}
+                            </Typography>
+
+                            <Button variant="contained" color="primary">
+                                Agregar al carrito
+                            </Button>
+                        </CardContent>
+                    </Grid>
+                </Grid>
+            </Card>
+        </Box>
     )
 }
