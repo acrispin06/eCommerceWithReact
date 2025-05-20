b6ebb22bb117b3b266752f3207138c74437d3895
acrispin06 <alvarocrispin0604@gmail.com>
2025-05-19 21:47:54 -0500

    feat: last cleaning code

--- src/cart/application/getCartByUser.ts
+++
@@ -1,5 +0,0 @@
-import type { Cart } from '../domain/Cart'
-
-export function getCartByUser(carts: Cart[], userId: number): Cart | undefined {
-    return carts.find(cart => cart.userId === userId)
-}
